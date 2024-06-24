ef593d2e7bfba9d75c1febbd2590427c35608e8b
Ana Clara Cordeiro <100931939+anaclaraxc@users.noreply.github.com>
2024-06-24 19:32:59 -0300

    API

---
+++ aula_api/api/config.php
@@ -0,0 +1,3 @@
+<?php
+    define('ACTIVE', true);
+    define('VERSION', '1.0.0');
---
+++ aula_api/api/date.php
@@ -0,0 +1,44 @@
+<?php
+    return [
+        [
+            'nome' => 'Ana Clara',
+            'email' => 'anaclara@gmail.com'
+        ],
+        [
+            'nome' => 'Danielle',
+            'email' => 'danielle@gmail.com'
+        ],
+        [
+            'nome' => 'Ana Laura',
+            'email' => 'analaura@gmail.com'
+        ],
+        [
+            'nome' => 'Lucas',
+            'email' => 'lucas@gmail.com'
+        ],
+        [
+            'nome' => 'Thifany',
+            'email' => 'thifany@gmail.com'
+        ],
+        [
+            'nome' => 'Ariany',
+            'email' => 'ariany@gmail.com'
+        ],
+        [
+            'nome' => 'Daniel',
+            'email' => 'daniel@gmail.com'
+        ],
+        [
+            'nome' => 'Maria Rita',
+            'email' => 'mariarita@gmail.com'
+        ],
+        [
+            'nome' => 'Stephanie ',
+            'email' => 'stephanie@gmail.com'
+        ],
+        [
+            'nome' => 'Maria Isadora',
+            'email' => 'mariaisadora@gmail.com'
+        ]
+      
+    ];
